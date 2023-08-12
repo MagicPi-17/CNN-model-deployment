8c052fa8b861cf3a15c30fb876d26b07c44b0d0b
MagicPi-17 <9martada9@gmail.com>
2023-08-13 00:32:24 +0300

    bug fixing & implementing the model

--- static/model.js
+++ static/model.js
@@ -3,9 +3,21 @@ const cachedModel = {
     loaded: false,
 };
 
-export async function predict(img) {
+export async function predict(file) {
+    return new Promise((resolve) => {
+      const img = new Image();
+      img.src = URL.createObjectURL(file);
+      img.onload = () => {
+        let tensor = tf.browser.fromPixels(img)
+          .resizeNearestNeighbor([224, 224])
+          .toFloat()
+          .div(tf.scalar(255.0))
+          .expandDims();
+        cachedModel.model.predict(tensor).data().then(rs => resolve(rs));
+      }
+    });
+  }
 
-}
 
 export async function loadModel() {
     const model = await tf.loadLayersModel('model/model.json');
